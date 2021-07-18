12d9844203657cab2e0b9ebe3786cd596cb1cb8c
AmirhosseinBidokhti <amir.bidokhti99@gmail.com>
2021-07-18 23:23:05 +0430

    moved collectionPreviews to overView to use ShopPage dynamically with Route API. Also added it's selector based on collectionName, and more...

--- src/components/collection-preview/collection-preview.jsx
+++ src/components/collection-preview/collection-preview.jsx
@@ -2,10 +2,18 @@ import React from "react";
 import "./collection-preview.styles.scss";
 
 import CollectionItem from "../collection-item/collection-item.component";
+import { withRouter } from "react-router-dom";
 
-const CollectionPreview = ({ title, items }) => (
+const CollectionPreview = ({ title, items, history, match, routeName }) => (
   <div className="collection-preview">
-    <h1 className="title">{title.toUpperCase()}</h1>
+    <h1
+      className="title"
+      onClick={() => {
+        history.push(`${match.path}/${routeName}`);
+      }}
+    >
+      {title.toUpperCase()}
+    </h1>
     <div className="preview">
       {items
         .filter((item, idx) => idx < 4)
@@ -16,7 +24,7 @@ const CollectionPreview = ({ title, items }) => (
   </div>
 );
 
-export default CollectionPreview;
+export default withRouter(CollectionPreview);
 
 // this function call chain of modifcation of array get called everytime as long as this component has to re-render.
 // going to be a performance concern if array gets large or on slower computers.
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,29 +1,18 @@
 import React from "react";
-import { connect } from "react-redux";
+import { Route } from "react-router-dom";
 
-import { createStructuredSelector } from "reselect";
-import { selectShopCollections } from "../../redux/shop/shop.selectors";
-
-import CollectionPreview from "../../components/collection-preview/collection-preview";
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
+import CollectionPage from "../collection/collection.component";
 
 import "./shop.styles.scss";
 
-class ShopPage extends React.Component {
-  render() {
-    const { collections } = this.props;
+const ShopPage = ({ match }) => {
+  return (
+    <div className="shop-page">
+      <Route exact path={`${match.path}`} component={CollectionsOverview} />
+      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+    </div>
+  );
+};
 
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
-
-const mapStateToProps = createStructuredSelector({
-  collections: selectShopCollections,
-});
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
